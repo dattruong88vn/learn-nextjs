be2ef8ffa04225ec87a5c78f25820b40f8faa50e
Dat Truong <dat.truong@propzy.com>
2022-03-13 14:39:30 +0700

    nextJS 02-05

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,16 @@
+import { useRouter } from "next/dist/client/router"
 import React from "react"
 
 export interface AboutProps {}
 
 export default function About(props: AboutProps) {
+  const router = useRouter()
+  console.log("about query: ", router.query)
   return <div>About page</div>
+}
+
+export async function getServerSideProps() {
+  return {
+    props: {},
+  }
 }
