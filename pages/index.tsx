d552e52dc5183cb0947ed94647864155824ccc89
Dat Truong <dat.truong@propzy.com>
2022-03-13 14:23:17 +0700

    nextJS 02-03: navigation between pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,23 @@
 import type { NextPage } from "next"
+import { useRouter } from "next/dist/client/router"
 import Head from "next/head"
 import Image from "next/image"
+import Link from "next/link"
 import styles from "../styles/Home.module.css"
 
 const Home: NextPage = () => {
+  const router = useRouter()
+
+  const goToDetailPage = () => {
+    router.push({
+      pathname: "posts/[postId]",
+      query: {
+        postId: 123,
+        ref: "social",
+      },
+    })
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -20,6 +34,12 @@ const Home: NextPage = () => {
         <p className={styles.description}>
           Get started by editing <code className={styles.code}>pages/index.tsx</code>
         </p>
+
+        <Link href='/about' passHref>
+          <a>Go to About</a>
+        </Link>
+
+        <button onClick={goToDetailPage}>Go to detail page</button>
 
         <div className={styles.grid}>
           <a href='https://nextjs.org/docs' className={styles.card}>
